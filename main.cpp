46a70b5a91a8d93a1b590d133e55ed91f911d854
Ole <ole.a.hetland@gmail.com>
2018-09-24 02:09:16 -0700

    Lagt til 2 nye brustyper

--- main.cpp
+++ main.cpp
@@ -21,10 +21,14 @@ int main()
 
     // Create a cola object. Constructor will be called with given parameters.
     Soda cola("Colabrus", 34, 4);
+    Soda fanta("Fantabrus", 25 , 5);
+    Soda redbull("Red Bull", 50, 5);
     // We have a variable named cola with Soda as its data type.
 
     // Call the VendingMachine addType function with cola variable as parameter.
     vendingMachine.addType(cola);
+    vendingMachine.addType(fanta);
+    vendingMachine.addType(redbull);
     // Print inventory using public printInventory member function (method).
     vendingMachine.printInventory();
 
